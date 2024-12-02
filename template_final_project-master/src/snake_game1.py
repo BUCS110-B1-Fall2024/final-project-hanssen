e16e1d551a2f4352122c4c97e7d3e95acf46f9d8
BuildTools <hly2@binghamton.edu>
2024-12-02 18:30:53 -0500

    final

--- template_final_project-master/src/snake_game1.py
+++ template_final_project-master/src/snake_game1.py
@@ -1,10 +1,25 @@
 import pygame 
-import time
+from pygame.locals import *
 
-def main():
+if __name__ == "__main__":
     pygame.init()
     
-    surface = pygame.display.set_mode((500, 500))
-    surface.fill((0, 0, 255))
+    surface = pygame.display.set_mode((1000, 500))
+    surface.fill((0, 128, 255))
     pygame.display.flip()
-    time.sleep(5)
+    
+    block = pygame.image.load("assets/spixil-frame-0.png").convert()
+    block_x = 1000
+    block_y = 500
+    surface.blit(block, (block_x, block_y))
+    
+    run = True
+    
+    while run:
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    run = False
+            elif event.type == QUIT:
+                run = False
+    
