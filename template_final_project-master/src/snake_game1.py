ed2f9a60d556092b80e31708284abaf8a66d7660
BuildTools <hly2@binghamton.edu>
2024-12-02 19:16:59 -0500

    3

--- template_final_project-master/src/snake_game1.py
+++ template_final_project-master/src/snake_game1.py
@@ -1,5 +1,6 @@
 import pygame 
 from pygame.locals import *
+import time
 
 class Snake:
     def __init__(self, parent_screen):
@@ -7,32 +8,41 @@ class Snake:
         self.block = pygame.image.load("template_final_project-master/assets/pixil-frame-0.png").convert()
         self.x = 100
         self.y = 100
+        self.direction = 'up'
         
     def draw(self):
         self.parent_screen.fill((0, 128, 255))
         self.parent_screen.blit(self.block, (self.x, self.y))
         pygame.display.flip()
         
-    def move_up(self):
-        self.y -= 10
+    def walk(self):
+        if self.direction == 'up':
+            self.y -= 10
+        if self.direction == 'down':
+            self.y += 10
+        if self.direction == 'left':
+            self.x -= 10
+        if self.direction == 'right':
+            self.x += 10
+            
         self.draw()
+        
+    def move_up(self):
+        self.direction = 'up'
         
     def move_down(self):
-        self.y += 10
-        self.draw()
+        self.direction = 'down'
         
     def move_left(self):
-        self.x -= 10
-        self.draw()
+        self.direction = 'left'
         
     def move_right(self):
-        self.x += 10
-        self.draw()
+        self.direction = 'right'
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.surface = pygame.display.set_mode((1000, 500))
+        self.surface = pygame.display.set_mode((500, 500))
         self.surface.fill((0, 128, 255))
         self.snake = Snake(self.surface)
         self.snake.draw()
@@ -57,7 +67,9 @@ class Game:
                 
                 elif event.type == QUIT:
                     run = False
-    
+                    
+            self.snake.walk() 
+            time.sleep(0.2)
 
 if __name__ == "__main__":
     game = Game()
