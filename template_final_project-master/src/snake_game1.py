9c376f902d9e2b8444acb2761c24e5e5f4a65066
BuildTools <hly2@binghamton.edu>
2024-12-02 20:30:10 -0500

    score

--- template_final_project-master/src/snake_game1.py
+++ template_final_project-master/src/snake_game1.py
@@ -1,6 +1,7 @@
 import pygame 
 from pygame.locals import *
 import time
+import random
 
 size = 40
 
@@ -12,9 +13,12 @@ class Apple:
         self.y = 120
         
     def draw(self):
-        self.parent_screen.fill((0, 128, 255))
         self.parent_screen.blit(self.image, (self.x, self.y))
         pygame.display.flip()
+        
+    def move(self):
+        self.x = random.randint(0, 24) * size
+        self.y = random.randint(0, 19) * size
 
 class Snake:
     def __init__(self, parent_screen, length):
@@ -23,7 +27,12 @@ class Snake:
         self.block = pygame.image.load("template_final_project-master/assets/pixil-frame-0.png").convert()
         self.x = [size] * length
         self.y = [size] * length
-        self.direction = 'up'
+        self.direction = 'down'
+        
+    def increase_length(self):
+        self.length+=1
+        self.x.append(-1)
+        self.y.append(-1)
         
     def draw(self):
         self.parent_screen.fill((0, 128, 255))
@@ -65,10 +74,32 @@ class Game:
         pygame.init()
         self.surface = pygame.display.set_mode((1000, 800))
         self.surface.fill((0, 128, 255))
-        self.snake = Snake(self.surface, 6)
+        self.snake = Snake(self.surface, 1)
         self.snake.draw()
         self.apple = Apple(self.surface)
         self.apple.draw()
+        
+    def is_collision(self, x1, y1, x2, y2):
+        if x1 >= x2 and x1 < x2 + size:
+            if y1 >= y2 and y1 < y2 + size:
+                return True
+            
+        return False
+        
+    def play(self):
+        self.snake.walk()
+        self.apple.draw()
+        self.display_score()
+        pygame.display.flip()
+        
+        if self.is_collision(self.snake.x[0], self.snake.y[0], self.apple.x, self.apple.y):
+            self.snake.increase_length()
+            self.apple.move()
+    
+    def display_score(self):
+        font = pygame.font.SysFont('roboto', 30, bold = True)
+        score = font.render(f"Score: {self.snake.length - 1}", True, (255, 255, 255))
+        self.surface.blit(score, (800, 10))
     
     def run(self):
         run = True
@@ -91,8 +122,7 @@ class Game:
                 elif event.type == QUIT:
                     run = False
                     
-            self.snake.walk() 
-            self.apple.draw()
+            self.play()
             
             time.sleep(0.3)
 
